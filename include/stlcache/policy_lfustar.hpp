7bdaf1c98eba65c237d166b07928e320adb82cd3
Denis Chapligin <akashihi@gmail.com>
2011-04-15 22:45:52 +0300

    Added copyright and license information

--- include/stlcache/policy_lfustar.hpp
+++ include/stlcache/policy_lfustar.hpp
@@ -1,3 +1,10 @@
+//
+// Copyright (C) 2011 Denis V Chapligin
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef STLCACHE_POLICY_LFUSTAR_HPP_INCLUDED
 #define STLCACHE_POLICY_LFUSTAR_HPP_INCLUDED
 
--- tests/test_lfuagingstar.cpp
+++ tests/test_lfuagingstar.cpp
@@ -1,3 +1,10 @@
+//
+// Copyright (C) 2011 Denis V Chapligin
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #define BOOST_TEST_MODULE "STLCachePolicyLFUAging*"
 
 #ifndef _MSC_VER
