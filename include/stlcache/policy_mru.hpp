c08af5fc447ae2eb3816283ad10c1fed8b420eea
Denis Chapligin <chollya@satgate.net>
2011-04-27 15:21:19 +0300

    Adapted lru/mru policies to bind style

--- include/stlcache/policy_mru.hpp
+++ include/stlcache/policy_mru.hpp
@@ -15,13 +15,20 @@ using namespace std;
 #include <stlcache/policy_lru.hpp>
 
 namespace stlcache {
-    template <class Key,template <typename T> class Allocator> class policy_mru : public policy_lru<Key,Allocator> {
+    template <class Key,template <typename T> class Allocator> class _policy_mru_type : public _policy_lru_type<Key,Allocator> {
     public:
-        policy_mru(const size_t& size ) throw() : policy_lru<Key,Allocator>(size) { }
+        _policy_mru_type(const size_t& size ) throw() : _policy_lru_type<Key,Allocator>(size) { }
 
         virtual const _victim<Key> victim() throw()  {
             return _victim<Key>(this->entries().front());
         }
+    };
+    struct policy_mru {
+        template <typename Key, template <typename T> class Allocator>
+            struct bind : _policy_mru_type<Key,Allocator> { 
+                bind(const bind& x) : _policy_mru_type<Key,Allocator>(x)  { }
+                bind(const size_t& size) : _policy_mru_type<Key,Allocator>(size) { }
+            };
     };
 }
 
