139b90150d9a2634c74299942182c72aecfce125
Denis Chaplygin <akashihi@gmail.com>
2023-05-03 19:17:16 +0300

    Migrated test_map to Catch2

---
+++ tests/test_main.cpp
@@ -0,0 +1,8 @@
+//
+// Copyright (C) 2011-2023 Denis V Chapligin, Martin Hrabovsky
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
