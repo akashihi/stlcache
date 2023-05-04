d9517514fb81e06bd18dec1f7241deadbdb4fe71
Denis Chaplygin <akashihi@gmail.com>
2023-05-04 19:45:37 +0300

    Migrated all lfu tests to Catch2. LFU-Aging intentionally left failing for now.

--- tests/test_lfu.cpp
+++ tests/test_lfu.cpp
@@ -1,54 +1,51 @@
 //
-// Copyright (C) 2011-2017 Denis V Chapligin, Martin Hrabovsky
+// Copyright (C) 2011-2023 Denis V Chapligin, Martin Hrabovsky
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#define BOOST_TEST_MODULE "STLCachePolicyLFU"
-#include <boost/test/unit_test.hpp>
-
+#include <catch2/catch_all.hpp>
 #include <stlcache/stlcache.hpp>
 
 using namespace stlcache;
+using namespace Catch::Matchers;
 
-BOOST_AUTO_TEST_SUITE(STLCacheSuite)
+TEST_CASE("LFU", "[policy,lfu]") {
+    SECTION("Default eviction") {
+        cache<int,string,policy_lfu> c1(3);
 
-BOOST_AUTO_TEST_CASE(lastInserted) {
-    cache<int,string,policy_lfu> c1(3);
+        c1.insert(1,"data1");
+        c1.insert(2,"data2");
+        c1.insert(3,"data3");
+        c1.insert(4,"data4");
 
-    c1.insert(1,"data1");
-    c1.insert(2,"data2");
-    c1.insert(3,"data3");
-    c1.insert(4,"data4");
+        REQUIRE(c1.size()==3);
+    }
 
-    BOOST_CHECK(c1.size()==3);
+    SECTION("String keys") {
+        cache<string, int, policy_lfu> c1(3);
+
+        c1.insert("data1",1);
+        c1.insert("data2",2);
+        c1.insert("data3",3);
+        c1.insert("data4",4);
+
+        REQUIRE(c1.size()==3);
+    }
+
+    SECTION("LFU eviction") {
+        cache<int,string,policy_lfu> c1(3);
+
+        c1.insert(1,"data1");
+        c1.insert(2,"data2");
+        c1.insert(3,"data3");
+
+        c1.touch(1);
+        c1.touch(3);
+
+        c1.insert(4,"data4");
+
+        CHECK_THROWS_AS(c1.fetch(2),exception_invalid_key); //Must be removed by LFU policy (cause 1&2 are touched)
+    }
 }
-
-BOOST_AUTO_TEST_CASE(stringAsKey) {
-    cache<string, int, policy_lfu> c1(3);
-
-    c1.insert("data1",1);
-    c1.insert("data2",2);
-    c1.insert("data3",3);
-    c1.insert("data4",4);
-
-    BOOST_CHECK(c1.size()==3);
-}
-
-BOOST_AUTO_TEST_CASE(touch) {
-    cache<int,string,policy_lfu> c1(3);
-
-    c1.insert(1,"data1");
-    c1.insert(2,"data2");
-    c1.insert(3,"data3");
-
-    c1.touch(1);
-    c1.touch(3);
-
-    c1.insert(4,"data4");
-
-    BOOST_REQUIRE_THROW(c1.fetch(2),exception_invalid_key); //Must be removed by LFU policy (cause 1&2 are touched)
-}
-
-BOOST_AUTO_TEST_SUITE_END();
--- tests/test_lfuaging.cpp
+++ tests/test_lfuaging.cpp
@@ -1,113 +1,100 @@
 //
-// Copyright (C) 2011-2017 Denis V Chapligin, Martin Hrabovsky
+// Copyright (C) 2011-2023 Denis V Chapligin, Martin Hrabovsky
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#define BOOST_TEST_MODULE "STLCachePolicyLFU-Aging"
-
-#ifndef _MSC_VER
-#include <unistd.h>
-#define WAIT_A_SECOND sleep(2);
-#else
-#include <windows.h>
-#define WAIT_A_SECOND Sleep(2000);
-#endif /* _MSC_VER */
-
-#include <boost/test/unit_test.hpp>
-
+#include <chrono>
+#include <thread>
+#include <catch2/catch_all.hpp>
 #include <stlcache/stlcache.hpp>
 
 using namespace stlcache;
+using namespace Catch::Matchers;
+using namespace std::chrono_literals;
 
-BOOST_AUTO_TEST_SUITE(STLCacheSuite)
+TEST_CASE("LFU Aging", "[policy,lfu]") {
+    SECTION("Default eviction") {
+        cache<int,string,policy_lfuaging<3600> > c1(3);
 
-BOOST_AUTO_TEST_CASE(lastInserted) {
+        c1.insert(1,"data1");
+        c1.insert(2,"data2");
+        c1.insert(3,"data3");
+        c1.insert(4,"data4");
 
-    cache<int,string,policy_lfuaging<3600> > c1(3);
+        REQUIRE(c1.size()==3);
+    }
 
-    c1.insert(1,"data1");
-    c1.insert(2,"data2");
-    c1.insert(3,"data3");
-    c1.insert(4,"data4");
+    SECTION("String keys") {
+        cache<string, int, policy_lfuaging<3600> > c1(3);
 
-    BOOST_CHECK(c1.size()==3);
+        c1.insert("data1",1);
+        c1.insert("data2",2);
+        c1.insert("data3",3);
+        c1.insert("data4",4);
+
+        REQUIRE(c1.size()==3);
+    }
+
+    SECTION("LFU eviction") {
+        cache<int,string,policy_lfuaging<3600> > c1(3);
+
+        c1.insert(1,"data1");
+        c1.insert(2,"data2");
+        c1.insert(3,"data3");
+
+        c1.touch(1);
+        c1.touch(2);
+
+        c1.insert(4,"data4");
+
+        CHECK_THROWS_AS(c1.fetch(3),exception_invalid_key); //Must be removed by LFU policy (cause 1&2 are touched)
+    }
+
+    SECTION("Items aging") {
+        cache<int,string,policy_lfuaging<1> > c1(3);
+
+        c1.insert(1,"data1");
+        c1.insert(2,"data2");
+        c1.insert(3,"data3");
+
+        c1.touch(1); //For key one refcount is 3 now
+        c1.touch(1);
+        c1.touch(1);
+        c1.touch(2); //For key two refcount is 3 now
+        c1.touch(2);
+        c1.touch(2);
+        c1.touch(3); //For key three refcount is 2 now
+        c1.touch(3);
+
+        std::this_thread::sleep_for(1100ms);
+
+        c1.insert(4,"data4");
+
+        CHECK_THROWS_AS(c1.fetch(3),exception_invalid_key); //Must be removed by LFU policy (cause every item have been touched and refcount for key 3 is 2)
+    }
+
+    SECTION("String keys aging") {
+        cache<string, int, policy_lfuaging<1> > c1(3);
+
+        c1.insert("data1",1);
+        c1.insert("data2",2);
+        c1.insert("data3",3);
+
+        c1.touch("data1"); //For key one refcount is 3 now
+        c1.touch("data1");
+        c1.touch("data1");
+        c1.touch("data2"); //For key two refcount is 3 now
+        c1.touch("data2");
+        c1.touch("data2");
+        c1.touch("data3"); //For key three refcount is 2 now
+        c1.touch("data3");
+
+        std::this_thread::sleep_for(1100ms);
+
+        c1.insert("data4", 4);
+
+        CHECK_THROWS_AS(c1.fetch("data3"),exception_invalid_key); //Must be removed by LFU policy (cause every item have been touched and refcount for key 3 is 2)
+    }
 }
-
-BOOST_AUTO_TEST_CASE(stringAsKey) {
-
-    cache<string, int, policy_lfuaging<3600> > c1(3);
-
-    c1.insert("data1",1);
-    c1.insert("data2",2);
-    c1.insert("data3",3);
-    c1.insert("data4",4);
-
-    BOOST_CHECK(c1.size()==3);
-}
-
-BOOST_AUTO_TEST_CASE(touch) {
-    cache<int,string,policy_lfuaging<3600> > c1(3);
-
-    c1.insert(1,"data1");
-    c1.insert(2,"data2");
-    c1.insert(3,"data3");
-
-    c1.touch(1);
-    c1.touch(2);
-
-    c1.insert(4,"data4");
-
-    BOOST_REQUIRE_THROW(c1.fetch(3),exception_invalid_key); //Must be removed by LFU policy (cause 1&2 are touched)
-}
-
-BOOST_AUTO_TEST_CASE(expire) {
-
-    cache<int,string,policy_lfuaging<1> > c1(3);
-
-    c1.insert(1,"data1");
-    c1.insert(2,"data2");
-    c1.insert(3,"data3");
-
-    c1.touch(1); //For key one refcount is 3 now
-    c1.touch(1);
-    c1.touch(1);
-    c1.touch(2); //For key two refcount is 3 now
-    c1.touch(2);
-    c1.touch(2);
-    c1.touch(3); //For key three refcount is 2 now
-    c1.touch(3);
-
-    WAIT_A_SECOND;
-
-    c1.insert(4,"data4");
-
-    BOOST_REQUIRE_THROW(c1.fetch(3),exception_invalid_key); //Must be removed by LFU policy (cause every item have been touched and refcount for key 3 is 2)
-}
-
-BOOST_AUTO_TEST_CASE(expireFoxString) {
-
-    cache<string, int, policy_lfuaging<1> > c1(3);
-
-    c1.insert("data1",1);
-    c1.insert("data2",2);
-    c1.insert("data3",3);
-
-    c1.touch("data1"); //For key one refcount is 3 now
-    c1.touch("data1"); 
-    c1.touch("data1");
-    c1.touch("data2"); //For key two refcount is 3 now
-    c1.touch("data2");
-    c1.touch("data2");
-    c1.touch("data3"); //For key three refcount is 2 now
-    c1.touch("data3");
-
-    WAIT_A_SECOND;
-
-    c1.insert("data4", 4);
-
-    BOOST_REQUIRE_THROW(c1.fetch("data3"),exception_invalid_key); //Must be removed by LFU policy (cause every item have been touched and refcount for key 3 is 2)
-}
-
-BOOST_AUTO_TEST_SUITE_END();
