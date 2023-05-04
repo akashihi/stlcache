696983736dc508106d89e2dffec8dc74b3b5235c
Denis Chaplygin <akashihi@gmail.com>
2023-05-04 21:14:26 +0300

    Migrated test_boost_optional to Catch2.

--- tests/test_boost_optional.cpp
+++ tests/test_boost_optional.cpp
@@ -1,35 +1,29 @@
 //
-// Copyright (C) 2011-2017 Denis V Chapligin, Martin Hrabovsky
+// Copyright (C) 2011-2023 Denis V Chapligin, Martin Hrabovsky
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#define BOOST_TEST_MODULE "STLCacheBoostOptional"
-#include <boost/test/unit_test.hpp>
-
+#include <catch2/catch_all.hpp>
+#include <boost/optional/optional_io.hpp>
 #define USE_BOOST_OPTIONAL
 #include <stlcache/stlcache.hpp>
 
 using namespace stlcache;
+using namespace Catch::Matchers;
 
-BOOST_AUTO_TEST_SUITE(STLCacheSuite)
-
-BOOST_AUTO_TEST_CASE(haveData) {
+TEST_CASE("Optional", "[map]") {
     cache<string,string,policy_none> c(10);
 
-    BOOST_CHECK(c.insert(string("key"),string("test"))); //Insert returns true for new entries
+    CHECK(c.insert(string("key"),string("test"))); //Insert returns true for new entries
 
-    BOOST_CHECK(c.get("key")); //Wrapper is not empty.
-    BOOST_CHECK(!c.get("key")->compare("test"));
+    SECTION("Fetch value") {
+        CHECK(c.get("key")); //Wrapper is not empty.
+        REQUIRE_THAT(*c.get("key"), Equals("test"));
+    }
+
+    SECTION("Fetch none") {
+        CHECK_FALSE(c.get("data")); //Wrapper is empty.
+    }
 }
-
-BOOST_AUTO_TEST_CASE(noData) {
-    cache<string,string,policy_none> c(10);
-
-    BOOST_CHECK(c.insert(string("key"),string("test"))); //Insert returns true for new entries
-
-    BOOST_CHECK(!c.get("data")); //Wrapper is empty.
-}
-
-BOOST_AUTO_TEST_SUITE_END();
