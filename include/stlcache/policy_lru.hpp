ff2566f6ffa956f45f7bd9a1711935aaa6236623
Denis Chaplygin <akashihi@gmail.com>
2023-05-06 16:19:22 +0300

    Simplified LRU code using modern language features

--- include/stlcache/policy_lru.hpp
+++ include/stlcache/policy_lru.hpp
@@ -10,37 +10,34 @@
 
 #include <list>
 
-using namespace std;
-
 #include <stlcache/policy.hpp>
 
 namespace stlcache {
     template <class Key,template <typename T> class Allocator> class _policy_lru_type : public policy<Key,Allocator> {
-        list<Key,Allocator<Key> > _entries;
-        typedef typename list<Key,Allocator<Key> >::iterator entriesIterator;
+        std::list<Key,Allocator<Key> > _entries;
+        typedef typename std::list<Key,Allocator<Key> >::iterator entriesIterator;
 
-        map<Key,entriesIterator,less<Key>,Allocator<pair<Key,entriesIterator> > > _entriesMap;
-        typedef typename map<Key,entriesIterator,less<Key>,Allocator<pair<Key,entriesIterator> > >::iterator entriesMapIterator;
+        std::map<Key,entriesIterator,less<Key>,Allocator<pair<Key,entriesIterator> > > _entriesMap;
     public:
         _policy_lru_type<Key,Allocator>& operator= ( const _policy_lru_type<Key,Allocator>& x) {
             this->_entries=x._entries;
             _entriesMap.clear();
-            for(entriesIterator entryIter=_entries.begin();entryIter!=_entries.end();++entryIter) {
-              _entriesMap.insert(pair<Key,entriesIterator>(*entryIter,entryIter));
+            for(auto entryIter=_entries.begin();entryIter!=_entries.end();++entryIter) {
+              _entriesMap.insert(std::make_pair(*entryIter,entryIter));
             }
             return *this;
         }
         _policy_lru_type(const _policy_lru_type<Key,Allocator>& x) {
             *this=x;
         }
-        _policy_lru_type(const size_t& size ) { }
+        explicit _policy_lru_type(const size_t& size ) { }
 
         virtual void insert(const Key& _k) {
-            entriesIterator entryIter = _entries.insert(_entries.begin(),_k);
-            _entriesMap.insert(pair<Key,entriesIterator>(_k,entryIter));
+            auto entryIter = _entries.insert(_entries.begin(),_k);
+            _entriesMap.insert(std::make_pair(_k,entryIter));
         }
         virtual void remove(const Key& _k) {
-            entriesMapIterator mapIter = _entriesMap.find(_k);
+            auto mapIter = _entriesMap.find(_k);
             if (mapIter==_entriesMap.end()) {
                 return;
             }
@@ -48,12 +45,12 @@ namespace stlcache {
             _entriesMap.erase(mapIter);
         }
         virtual void touch(const Key& _k) {
-            entriesMapIterator mapIter = _entriesMap.find(_k);
+            auto mapIter = _entriesMap.find(_k);
             if (mapIter==_entriesMap.end()) {
                 return;
             }
             _entries.erase(mapIter->second);
-            entriesIterator entryIter = _entries.insert(_entries.begin(),_k);
+            auto entryIter = _entries.insert(_entries.begin(),_k);
             mapIter->second=entryIter;
         }
         virtual void clear() {
@@ -62,7 +59,7 @@ namespace stlcache {
         }
         virtual void swap(policy<Key,Allocator>& _p) {
             try {
-                _policy_lru_type<Key,Allocator>& _pn=dynamic_cast<_policy_lru_type<Key,Allocator>& >(_p);
+                auto& _pn=dynamic_cast<_policy_lru_type<Key,Allocator>& >(_p);
                 _entries.swap(_pn._entries);
                 _entriesMap.swap(_pn._entriesMap);
             } catch (const std::bad_cast& ) {
@@ -92,7 +89,7 @@ namespace stlcache {
         template <typename Key, template <typename T> class Allocator>
             struct bind : _policy_lru_type<Key,Allocator> {
                 bind(const bind& x) : _policy_lru_type<Key,Allocator>(x)  { }
-                bind(const size_t& size) : _policy_lru_type<Key,Allocator>(size) { }
+                explicit bind(const size_t& size) : _policy_lru_type<Key,Allocator>(size) { }
             };
     };
 }
